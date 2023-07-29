6dbd43b912ec02bba2ec26d6aef546fd0f80a771
Franco Marcellini <francomarcellini@gmail.com>
2023-07-28 23:17:42 -0300

    entrega beta 3

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics"
 import {getFirestore} from "firebase/firestore";
 
 
 // "AIzaSyDJOaW84u0vhqY-J1O3E-O0zNztIoInBuU"
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "coderhouse-d25aa.firebaseapp.com",
   projectId: "coderhouse-d25aa",
@@ -12,8 +13,8 @@ const firebaseConfig = {
   messagingSenderId: "996032839105",
   appId: "1:996032839105:web:35c9987c86c849ae7d0340"
 };
-
-/* const firebaseConfig = {
+ */
+const firebaseConfig = {
   apiKey: "AIzaSyDV9ns9oX0AFOq_wYQIkas8_zu74311CZA",
   authDomain: "geek-n-resin.firebaseapp.com",
   projectId: "geek-n-resin",
@@ -22,7 +23,7 @@ const firebaseConfig = {
   appId: "1:463725320467:web:e23622e548e43cd803a2e6",
   measurementId: "G-EEBGGD4GQB"
 };
- */
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
